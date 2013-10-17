4afa58cf6e7c10fcb88e4b1a3df67545afc64006
Lisa P <passinglisa@gmail.com>
2013-10-17 23:39:40 +0200

    navigation scroll + tweetlink

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+  var method;
+  var noop = function () {};
+  var methods = [
+    'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+    'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+    'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+    'timeStamp', 'trace', 'warn'
+  ];
+  var length = methods.length;
+  var console = (window.console = window.console || {});
 
+  while (length--) {
+    method = methods[length];
+
+    // Only stub undefined methods.
+    if (!console[method]) {
+      console[method] = noop;
+    }
+  }
+}());
+
+$(function() {
+  $('.header a:not(.external)').on('click', function(e) {
+    e.preventDefault();
+    var to = $(this).attr('href');
+    $('html, body').animate({scrollTop: $(to).offset().top }, '5000');
+  });
+})
